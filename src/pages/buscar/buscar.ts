af419fdab6500311391b68c7e7a11b4e61cdacc0
Valentina <valentinav@unicauca.edu.co>
2020-12-20 04:01:27 -0500

    acomodar html buscar

--- src/pages/buscar/buscar.ts
+++ src/pages/buscar/buscar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { referenciasService } from '../../services/referenciasServices';
 
 /**
  * Generated class for the BuscarPage page.
@@ -15,7 +16,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BuscarPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public refService: referenciasService) {
   }
 
   ionViewDidLoad() {
